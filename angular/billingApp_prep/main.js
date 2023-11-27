8e873d3a1ff92ba51e0c368a6eb3be06f8ac2541
Gregory-reyes <55810861+Gregory-reyes@users.noreply.github.com>
2023-11-27 16:22:04 +0000

    Prueba de con localhost  ya qie todas las direcciones no funciono

--- angular/billingApp_prep/main.js
+++ angular/billingApp_prep/main.js
@@ -53,7 +53,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    basePath: 'http://0.0.0.0:7080'
+    basePath: 'http://localhost:7080'
 };
 /*
  * For easier debugging in development mode, you can import the following file
